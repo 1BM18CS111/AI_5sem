850d57c096c04d9376924963ebf5ebdd7b9c6acc
1BM18CS111 <53754240+1BM18CS111@users.noreply.github.com>
2020-11-02 10:49:48 +0530

    Update code.py

--- lab5/code.py
+++ lab5/code.py
@@ -1,1 +1,12 @@
-d
+def clean(floor):
+    for x in range(len(floor)):
+        print('Floor {} : '.format(x+1))
+        for y in range(len(floor[0])):
+            floor = print_floor(floor, x, y)
+        print()
+def print_floor(floor, row, col): 
+    if floor[row][col]:
+        floor[row][col] = 0
+        print(' Cleaned ', end = ' ')
+    else: print(' Already Cleaned ', end = ' ')
+    return floor
